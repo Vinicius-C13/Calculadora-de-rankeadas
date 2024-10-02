628d3549e23ecaeffa58b634fa9d629a35c5f0b1
Vinícius Costa <96350207+Vinicius-C13@users.noreply.github.com>
2024-10-02 16:05:34 -0300

    Fix: Fix the function output when input is not a number

--- calculadora-de-rankeadas.js
+++ calculadora-de-rankeadas.js
@@ -1,24 +1,25 @@
 function calculadora_de_rankeadas(vitorias){
 
-    let tier
+    let tier = ""
     //Estrutura condicional que define o output se baseando no valor do input
     if(typeof vitorias != "number") {
       console.log("Insira um valor válido!")
-    } else if(vitorias < 10) {  
-      tier = "Ferro"
-    } else if(vitorias < 20) {
-        tier = "Bronze"
-    } else if(vitorias < 50) {
-        tier = "Prata"
-    } else if(vitorias < 80) {
-        tier = "Ouro"
-    } else if(vitorias < 90) {
-        tier = "Diamante"
-    } else if(vitorias < 100) {
-        tier = "Lendário"
     } else {
-        tier = "Imortal"
+        if(vitorias < 10) {  
+          tier = "Ferro"
+        } else if(vitorias < 20) {
+          tier = "Bronze"
+        } else if(vitorias < 50) {
+          tier = "Prata"
+        } else if(vitorias < 80) {
+          tier = "Ouro"
+        } else if(vitorias < 90) {
+          tier = "Diamante"
+        } else if(vitorias < 100) {
+          tier = "Lendário"
+        } else {
+          tier = "Imortal"
+        }
+        console.log(`O jogador tem ${vitorias} vitorias e seu tier é ${tier}`)
     }
-
-    console.log(`O jogador tem ${vitorias} vitorias e seu tier é ${tier}`)
 }
